010ecb31ae178d55922c90de8f6cbad0efc3f541
JimmyIL <49790040+JimmyIL@users.noreply.github.com>
2023-03-29 17:31:10 -0500

    Update import-aws_iam_roles.ps1

--- import-aws_iam_roles.ps1
+++ import-aws_iam_roles.ps1
@@ -1,57 +1,48 @@
-function import-iam_roles { 
-
-  #set default region, if not set this uses us-west-2  < import-iam_roles -region us-east-1  >  (example usage)
-  param (
-    $region = "us-east-1" 
-  )
-
-  # create necessary folder and files for import
-  $r = 'aws_iam_role'          #resource we are using 
-  $roles = (Get-IAMRoleList).RoleName
-
-  #provider info
+function import-aws_iam_role { 
+  param( 
+    #location to put all the resources/files in.
+    $yourpath = "C:\Users\liljohn\folderdirwiththings" ,
+    #aws provider version being used
+    $providerversion = "3.55" ,
+    #region of where resources are located.
+    $region = "us-east-1" ,
+    #resource type in terraform 'resource {}' reference we are importing. (i.e. aws_instance, aws_iam_user, aws_alb )
+    $tfresourcetype = "aws_iam_role"
+  ) 
+  
+  Set-Location -Path $yourpath &&
+  New-Item -ItemType Directory -Path "$pwd\$tfresourcetype"   
   $provider_for_main = @"
-// Provider configuration
-terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = ">= 3.0"
+  // Provider configuration
+  terraform {
+   required_providers {
+     aws = {
+       source  = "hashicorp/aws"
+       version = "~> $providerversion"
+     }
    }
- }
-}
- provider "aws" {
- region = "$region"
- }
-"@
-
-  #creates necessary files and folders for importing.
-  mkdir ".\$r"
-  New-Item -Path ".\$r\imported_$r.tf" 
-  New-Item -Path ".\imported_$r.tf" 
-  New-Item -Path ".\main.tf"  
-  New-Item -Path ".\terraform.tfstate" 
-  New-Item -Path ".\$r\variables.tfvars"
-
-
-  $providerinfo = $provider_for_main | Out-File -FilePath ".\main.tf" -Append 
-  $providerinfo && Invoke-Command { terraform init }   #init is needed prior to import
-
-  #foreach role found do this
-  foreach ($role in $roles) {
-    $Roleid = (Get-IAMRole -RoleName $role).RoleId 
-    
-    $resourceconfig = @"
-resource "$r" "$Roleid" { 
-} 
-"@
-    $resourcecopy = $resourceconfig | Out-File -Path ".\imported_$r.tf" -Append
-    $invokethis = Invoke-Command { terraform import aws_iam_role.$Roleid $role } -Verbose
-    $copystatefile = Invoke-Command { terraform state show -no-color aws_iam_role.$Roleid } | Out-File -Path ".\$r\imported_$r.tf" -Append 
-    $resourcecopy && $invokethis && $copystatefile
-
   }
-  $name = (Get-Content -Path ".\$r\imported_$r.tf") | Select-String -SimpleMatch "="
-  $maketfvars = $name | Set-Content -Path ".\$r\variables.tfvars"
-  $maketfvars && Remove-Item -Path ".\imported_$r.tf" -Force
+   
+  provider "aws" {
+   region = "$region"
+  }
+"@
+  
+  Set-Content -Path "$pwd\$tfresourcetype\provider.tf" -Value $provider_for_main -Force && 
+  Set-Location -Path "$pwd\$tfresourcetype" && 
+  Invoke-Command { terraform init }
+  
+  $ids = (Get-IamRoleList -region $region).RoleName | Select-Object -Unique 
+  
+  #foreach found do this
+  foreach ($number in $ids) { 
+    $resourcetags = (Get-IamRole -Region $region -RoleName $number).RoleName 
+    $resourceid = $resourcetags -split " ", ""  
+    $randomness = Write-Output ( -join ((0x30..0x39) + ( 0x41..0x5A) + ( 0x61..0x7A) | Get-Random -Count 6 | % { [char]$_ }) )
+    if ([string]::IsNullOrWhitespace($resourceid)) { $resourceid = $number } 
+    Invoke-Command { terraform import -allow-missing-config $tfresourcetype'.'$resourceid'-'$randomness $number } 
+  } 
+  
+  $thisthing = Invoke-Command { terraform show -no-color }  
+  $thisthing | Add-Content -Path "$pwd\imported_$tfresourcetype.tf"
 } 
